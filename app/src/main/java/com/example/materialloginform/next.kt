994538405d8ff39936e205153fdf83e7bd596368
Waigwa254 <augustinewaigwa254@gmail.com>
2022-12-10 05:14:04 -1000

    updates

---
+++ app/src/main/java/com/example/materialloginform/next.kt
@@ -0,0 +1,11 @@
+package com.example.materialloginform
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class next : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_next)
+    }
+}
